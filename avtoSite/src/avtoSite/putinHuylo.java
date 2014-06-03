7c45e45e439b92f71b5f7cb331c16588b71e8524
ANAKONDA <ANAKONDA@ANAKONDA-PC>
2014-06-03 21:49:57 +0300

    update - putin huylo

--- avtoSite/src/avtoSite/putinHuylo.java
+++ avtoSite/src/avtoSite/putinHuylo.java
@@ -8,7 +8,7 @@ public class putinHuylo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.Out.Print("Putin Huylo!!!");
+            System.out.print("Putin Huylo!!!");
 	}
 
 }
