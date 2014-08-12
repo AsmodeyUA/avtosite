9470529b215ca0ec9b308e99b46a594b3cc5706d
ANAKONDA <ANAKONDA@ANAKONDA-PC>
2014-08-12 23:03:36 +0300

    2sss

--- avtoSite/src/avtoSite/import_obig1c.java
+++ avtoSite/src/avtoSite/import_obig1c.java
@@ -11,7 +11,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class import_obig1c {
-	
+
 	public import_obig1c(String name) {
 		excelfilenameread = name;
 		in = null;
@@ -34,28 +34,47 @@ public class import_obig1c {
 	protected String excelfilenameread;
 	protected InputStream in;
 	protected static HSSFWorkbook wb;
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-//	public void ReadRows(category[] b, int parent_id, int colcategory, int startpos) {
-	public void ReadRows( product[] prod, int colcategory, int startpos) {
+	//	public void ReadRows(category[] b, int parent_id, int colcategory, int startpos) {
+	public void ReadRows( product[] prod, int man_id, int parent_cat, int startpos, 
+			int colname, int coldescr, int colprice, int colcount) {
 		Sheet sheet = wb.getSheetAt(0);
 		Iterator<Row> it = sheet.iterator();
 		String temp_storing = "xxx";
 		int colRow = 0;
-				
 		while (it.hasNext()) {
 			Row row = it.next();
-			colRow++;
-			if (colRow > startpos) {
+			colRow++; int k=0;
+			String name="";
+			String description="";
+			String price="";
+			String count="";
+
+
+			if ((colRow > startpos)&&(colRow < 100)) {
 				Iterator<Cell> cells = row.iterator();
 				while (cells.hasNext()) {
+					k++;
 					Cell cell = cells.next();
 					int cellType = cell.getCellType();
-					
+					if (k == colname) {
+						name = cell.toString();
+					}
+					if (k == coldescr) {
+						description = cell.toString();
+					}		
+					if (k == colprice) {
+						price = cell.toString();
+					}
+					if (k == colcount) {
+						count = cell.toString();
+					}					
+
 					switch (cellType) {
 					case Cell.CELL_TYPE_STRING:
 						temp_storing = cell.getStringCellValue();
@@ -67,9 +86,22 @@ public class import_obig1c {
 						temp_storing = "";
 						break;
 					}
-					System.out.println(temp_storing);
+					//System.out.println(k+"  "+temp_storing);
 				}
-
+				if (name!="") {
+					System.out.println(colRow+" "+name+"  "+description+" "+price+ " "+count);
+					System.out.println(product.getMax_id());
+					int prod_id = product.getMax_id();
+					prod[prod_id] = new product(name);
+					prod[prod_id].setName(name);
+					// System.out.println(p[prod_id].getName());
+					prod[prod_id].setModel(name);
+					prod[prod_id].setDescription(description);
+					prod[prod_id].setQuantity(count);
+					prod[prod_id].setPrice(price);
+					prod[prod_id].setManufacturer_id(man_id);
+					prod[prod_id].setCategory_id(parent_cat);
+				}
 			}
 		}
 	}
--- avtoSite/src/avtoSite/putinHuylo.java
+++ avtoSite/src/avtoSite/putinHuylo.java
@@ -1,6 +1,5 @@
 package avtoSite;
 
-
 public class putinHuylo {
 
 	public putinHuylo() {
@@ -9,12 +8,24 @@ public class putinHuylo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            System.out.print("Putin Huylo!!!");
-            String path = new String("/local/proba/3007.xls");
-            import_obig1c Data3007 = new import_obig1c(path);
-            product[] b = null;
-            Data3007.open();
-            Data3007.ReadRows(b,2,9);
+		System.out.print("Putin Huylo!!!");
+		category[] categ=new category[1000];
+		//            String path = new String("/local/proba/3007.xls");
+		String path = new String("D:\\work\\Dani\\3007.xls");
+		int q=category.getMax_id(); 
+		categ[q]=new category("Загальний");  	
+		int startpos=9;
+		int manufacturer=0;
+		int parent=categ[q].getId(); 
+		int colname=1; //artikul
+		int coldescr=4;
+		int colprice=10;
+		int colcount=14;
+
+		import_obig1c Data3007 = new import_obig1c(path);
+		product[] prod = new product[20000];
+		if (Data3007.open())
+			Data3007.ReadRows(prod,manufacturer,parent,startpos,colname,coldescr,colprice,colcount);
 	}
 
 }
